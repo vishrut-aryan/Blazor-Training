7be140953dd83f472898f498bc1847534cffa0e8
Vishrut Aryan <VAryan@holtecasia.com>
2024-09-09 17:06:56 +0530

    more progress

---
+++ HiddenVilla2/HiddenVilla2/Model/BlazorRoom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace HiddenVilla2.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+        public List<BlazorRoomProp> RoomProps { get; set; }
+
+    }
+}
---
+++ HiddenVilla2/HiddenVilla2/Model/BlazorRoomProp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HiddenVilla2.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
