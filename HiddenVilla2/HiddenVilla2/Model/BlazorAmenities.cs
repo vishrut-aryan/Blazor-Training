7a7ad4ecda07e13dafa9a5ed4c5804239602570c
Vishrut Aryan <VAryan@holtecasia.com>
2024-09-13 13:03:48 +0530

    koiyutfdf today is not really working

---
+++ HiddenVilla2/HiddenVilla2/Model/BlazorAmenities.cs
@@ -0,0 +1,9 @@
+﻿namespace HiddenVilla2.Model
+{
+    public class BlazorAmenities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
